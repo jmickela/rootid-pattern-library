cb9a4b95c24b6aa5f7514a80207d58279161dfda
Jason Mickela <jason@rootid.in>
2019-01-08 15:51:27 -0800

    renamed colors and fixed template paths

--- patterns/js/site.js
+++ patterns/js/site.js
@@ -1,5 +1,30 @@
 (function(window, document, $) {
 'use strict';
+$(".navmenu-offcanvas .menu-item .dropdown-toggle").on('click', function () {
+
+  $(this).closest('.navmenu-offcanvas .menu-item').toggleClass('open');
+});
+$(document).ready(function () {
+  
+  $('.expandingsearch__button').on('click', function () {
+    var $parent = $(this).parent();
+
+    if ($parent.hasClass('expandingsearch--open') &&
+        $('.expandingsearch__input', $parent).val() == '') {
+      $parent.removeClass('expandingsearch--open');
+      $('.expandingsearch__input', $parent).off( "focus" );
+      return false;
+    } else if($parent.hasClass('expandingsearch--open')) {
+      return true;
+    }
+
+    $(this).parent().toggleClass('expandingsearch--open');
+    $('.expandingsearch__input', $parent).focus();
+    return false;
+  });
+
+});
+
 
 $('.card1--person').on('click', function() {
   let $wrapper = $(".person-popup__wrapper", this);
@@ -93,31 +118,6 @@ $(window).scroll(headerScroll);
 //   }
 // });
 
-
-$(".navmenu-offcanvas .menu-item .dropdown-toggle").on('click', function () {
-
-  $(this).closest('.navmenu-offcanvas .menu-item').toggleClass('open');
-});
-$(document).ready(function () {
-  
-  $('.expandingsearch__button').on('click', function () {
-    var $parent = $(this).parent();
-
-    if ($parent.hasClass('expandingsearch--open') &&
-        $('.expandingsearch__input', $parent).val() == '') {
-      $parent.removeClass('expandingsearch--open');
-      $('.expandingsearch__input', $parent).off( "focus" );
-      return false;
-    } else if($parent.hasClass('expandingsearch--open')) {
-      return true;
-    }
-
-    $(this).parent().toggleClass('expandingsearch--open');
-    $('.expandingsearch__input', $parent).focus();
-    return false;
-  });
-
-});
 
 // $(document).ready(function() {
 //   var slider = tns({
