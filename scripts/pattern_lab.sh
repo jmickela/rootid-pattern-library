fc070658a3a68e3757c118d69d0da8c24f7f4d26
Jason Mickela <jason@rootid.in>
2019-01-09 11:15:02 -0800

    added twig namespaces plugin

--- scripts/pattern_lab.sh
+++ scripts/pattern_lab.sh
@@ -3,9 +3,11 @@
 rm -rf .pattern-lab
 
 composer create-project -n pattern-lab/edition-twig-standard .pattern-lab
+cd .pattern-lab
+composer require evanlovely/plugin-twig-namespaces
 
 # Delete the default source directory
-rm -rf .pattern-lab/source
+rm -rf source
 
 # Symlink our components directory to the source location we just deleted
-ln -s ../patterns/ .pattern-lab/source
+ln -s ../patterns/ source
