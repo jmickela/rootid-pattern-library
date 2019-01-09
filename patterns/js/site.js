64b92891cde2bc069aac25f91a43e6972e148323
Jason Mickela <jason@rootid.in>
2019-01-08 16:24:01 -0800

    some fixes and started card grid

--- patterns/js/site.js
+++ patterns/js/site.js
@@ -1,63 +1,5 @@
 (function(window, document, $) {
 'use strict';
-$(".navmenu-offcanvas .menu-item .dropdown-toggle").on('click', function () {
-
-  $(this).closest('.navmenu-offcanvas .menu-item').toggleClass('open');
-});
-$(document).ready(function () {
-  
-  $('.expandingsearch__button').on('click', function () {
-    var $parent = $(this).parent();
-
-    if ($parent.hasClass('expandingsearch--open') &&
-        $('.expandingsearch__input', $parent).val() == '') {
-      $parent.removeClass('expandingsearch--open');
-      $('.expandingsearch__input', $parent).off( "focus" );
-      return false;
-    } else if($parent.hasClass('expandingsearch--open')) {
-      return true;
-    }
-
-    $(this).parent().toggleClass('expandingsearch--open');
-    $('.expandingsearch__input', $parent).focus();
-    return false;
-  });
-
-});
-
-
-$('.card1--person').on('click', function() {
-  let $wrapper = $(".person-popup__wrapper", this);
-  let $content = $(".person-popup", $wrapper);
-
-  if(!$wrapper.hasClass('open')) {
-    $wrapper.addClass('open');
-  }
-
-  return false;
-});
-
-$(".person-popup__wrapper").on('click', function (){
-  closePersonPopup();
-  return false;
-});
-
-$(".person-popup__close").on('click', function () {
-  closePersonPopup();
-  return false;
-});
-
-$(document).keyup(function(e) {
-  if (e.keyCode === 27) closePersonPopup();   // esc
-});
-
-function closePersonPopup($element) {
-  $(".person-popup__wrapper.open").removeClass('open');
-}
-
-$('.person-popup').on('click', function(e) {
-  e.stopImmediatePropagation();
-});
 function headerScroll() {
   let scrollHeight = $(window).scrollTop();
   let scrollCutoff = 10;
@@ -119,6 +61,86 @@ $(window).scroll(headerScroll);
 // });
 
 
+
+$('.card1--person').on('click', function() {
+  let $wrapper = $(".person-popup__wrapper", this);
+  let $content = $(".person-popup", $wrapper);
+
+  if(!$wrapper.hasClass('open')) {
+    $wrapper.addClass('open');
+  }
+
+  return false;
+});
+
+$(".person-popup__wrapper").on('click', function (){
+  closePersonPopup();
+  return false;
+});
+
+$(".person-popup__close").on('click', function () {
+  closePersonPopup();
+  return false;
+});
+
+$(document).keyup(function(e) {
+  if (e.keyCode === 27) closePersonPopup();   // esc
+});
+
+function closePersonPopup($element) {
+  $(".person-popup__wrapper.open").removeClass('open');
+}
+
+$('.person-popup').on('click', function(e) {
+  e.stopImmediatePropagation();
+});
+$(".navmenu-offcanvas .menu-item .dropdown-toggle").on('click', function () {
+
+  $(this).closest('.navmenu-offcanvas .menu-item').toggleClass('open');
+});
+$(document).ready(function () {
+  
+  $('.expandingsearch__button').on('click', function () {
+    var $parent = $(this).parent();
+
+    if ($parent.hasClass('expandingsearch--open') &&
+        $('.expandingsearch__input', $parent).val() == '') {
+      $parent.removeClass('expandingsearch--open');
+      $('.expandingsearch__input', $parent).off( "focus" );
+      return false;
+    } else if($parent.hasClass('expandingsearch--open')) {
+      return true;
+    }
+
+    $(this).parent().toggleClass('expandingsearch--open');
+    $('.expandingsearch__input', $parent).focus();
+    return false;
+  });
+
+});
+
+if($('.instagram-feed').length !== 0) {
+  $('.instagram-feed').each(function ($index) {
+    let username = $(this).attr('data-instagram-username');
+    let $that = $(this);
+
+    if(username !== undefined && username !== "") {
+      $.get('https://www.instagram.com/' + username, null, function (data) {
+        let temp = data.split("window._sharedData = ");
+        temp = temp[1].split(";<\/script>");
+        const results = JSON.parse(temp[0]);
+
+        for (let i = 0; i < 9; i++) {
+          let item = results['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['edges'][i]['node'];
+          let url = item['thumbnail_resources'][0]['src'];
+          $that.append("<a class='instagram-feed__item' target='_blank' href='https://www.instagram.com/p/" + item.shortcode + "'><img src='" + url + "' /></a>");
+        }
+      });
+    }
+  });
+}
+
+
 // $(document).ready(function() {
 //   var slider = tns({
 //     container: '.cardslider__cards',
@@ -150,28 +172,6 @@ $('.cardslider__cards').slick({
     }
   ]
 });
-if($('.instagram-feed').length !== 0) {
-  $('.instagram-feed').each(function ($index) {
-    let username = $(this).attr('data-instagram-username');
-    let $that = $(this);
-
-    if(username !== undefined && username !== "") {
-      $.get('https://www.instagram.com/' + username, null, function (data) {
-        let temp = data.split("window._sharedData = ");
-        temp = temp[1].split(";<\/script>");
-        const results = JSON.parse(temp[0]);
-
-        for (let i = 0; i < 9; i++) {
-          let item = results['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['edges'][i]['node'];
-          let url = item['thumbnail_resources'][0]['src'];
-          $that.append("<a class='instagram-feed__item' target='_blank' href='https://www.instagram.com/p/" + item.shortcode + "'><img src='" + url + "' /></a>");
-        }
-      });
-    }
-  });
-}
-
-
 $('.strategicpartnerships__partners').slick({
   infinite: true,
   slidesToShow: 5,
