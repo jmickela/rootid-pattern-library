952c7f6757978446c425bf984d2819c2655bcba5
Jason Mickela <jason@rootid.in>
2019-01-14 15:28:45 -0800

    updates

--- dist/js/site.js
+++ dist/js/site.js
@@ -1,26 +1,5 @@
 (function(window, document, $) {
 'use strict';
-$(document).ready(function () {
-  
-  $('.expandingsearch__button').on('click', function () {
-    var $parent = $(this).parent();
-
-    if ($parent.hasClass('expandingsearch--open') &&
-        $('.expandingsearch__input', $parent).val() == '') {
-      $parent.removeClass('expandingsearch--open');
-      $('.expandingsearch__input', $parent).off( "focus" );
-      return false;
-    } else if($parent.hasClass('expandingsearch--open')) {
-      return true;
-    }
-
-    $(this).parent().toggleClass('expandingsearch--open');
-    $('.expandingsearch__input', $parent).focus();
-    return false;
-  });
-
-});
-
 function headerScroll() {
   let scrollHeight = $(window).scrollTop();
   let scrollCutoff = 10;
@@ -119,6 +98,27 @@ $(".navmenu-offcanvas .menu-item .dropdown-toggle").on('click', function () {
 
   $(this).closest('.navmenu-offcanvas .menu-item').toggleClass('open');
 });
+$(document).ready(function () {
+  
+  $('.expandingsearch__button').on('click', function () {
+    var $parent = $(this).parent();
+
+    if ($parent.hasClass('expandingsearch--open') &&
+        $('.expandingsearch__input', $parent).val() == '') {
+      $parent.removeClass('expandingsearch--open');
+      $('.expandingsearch__input', $parent).off( "focus" );
+      return false;
+    } else if($parent.hasClass('expandingsearch--open')) {
+      return true;
+    }
+
+    $(this).parent().toggleClass('expandingsearch--open');
+    $('.expandingsearch__input', $parent).focus();
+    return false;
+  });
+
+});
+
 // $(document).ready(function() {
 //   var slider = tns({
 //     container: '.cardslider__cards',
