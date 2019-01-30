340560aa8d00821ca2f869b1e9ee35ea17cf97cd
Meg Claypool <margaret.claypool@gmail.com>
2019-01-30 12:22:47 -0800

    Work on menus and offcanvas -- in progress

--- patterns/js/site.js
+++ patterns/js/site.js
@@ -34,6 +34,25 @@ $(document).ready(function () {
 });
 
 (function() {
+  let hamburgerToggles = document.querySelectorAll('.hamburger-toggle');
+
+  for(var i = 0; i < hamburgerToggles.length; i++) {
+    hamburgerToggles[i].addEventListener('click', toggleHamburger);
+  }
+
+  function toggleHamburger(e) {
+    console.log("i clicked!");
+    console.log(e.target);
+
+    if(e.target.classList.contains('hamburger-toggle')) {
+      e.target.classList.toggle('open');
+    } else {
+      e.target.parentNode.classList.toggle('open');
+    }
+    
+  }
+})();
+(function() {
   let offCanvasButtons = document.querySelectorAll('[data-offcanvas]');
   let offCanvasClassName = offCanvasButtons[0].getAttribute('data-offcanvas');
   let offCanvasCloseButton = document.querySelector('.offcanvas__header .offcanvas__close');
